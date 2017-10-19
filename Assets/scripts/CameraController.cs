a55608a2d18377f6f04e01493b5065877fbb396c
Felix Holz <felix.holz@me.com>
2017-10-19 09:42:47 +0200

    move red block

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+    
+	void Awake () {
+        offset = this.transform.position - player.transform.position;
+	}
+	
+	void FixedUpdate () {
+        this.transform.position = player.transform.position + offset;
+	}
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 2.0f;
+    public float turnspeed = 180;
+
+    private Rigidbody body;
+    private float moveInput;
+    private float turnInput;
+
+	// Use this for initialization
+	void Awake () {
+        body = GetComponent<Rigidbody>();
+	}
+
+    private void OnEnable()
+    {
+        body.isKinematic = false;
+        turnInput = 0;
+        moveInput = 0;
+    }
+
+    private void OnDisable()
+    {
+        body.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        moveInput = Input.GetAxis("Vertical");
+        turnInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        //move
+
+        Vector3 delta = this.transform.forward * moveInput * speed * Time.deltaTime;
+        body.MovePosition(body.position + delta);
+
+        //turn
+        float turnDegrees = turnInput * turnspeed * Time.deltaTime;
+        Quaternion rotation = Quaternion.Euler(0.0f, turnDegrees, 0.0f);
+        body.MoveRotation(body.rotation * rotation);
+
+    }
+}

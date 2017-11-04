e8997068f823cccdc3bad8ae84b684fa91d5fbe7
Felix Holz <felix.holz@me.com>
2017-11-04 19:14:55 +0100

    very basic main menu

---
+++ Assets/scripts/ui/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+    public void BtnTempScene()
+    {
+        SceneManager.LoadScene("temp", LoadSceneMode.Single);
+    }
+
+    public void BtnLvl1()
+    {
+        SceneManager.LoadScene("map1", LoadSceneMode.Single);
+    }
+}

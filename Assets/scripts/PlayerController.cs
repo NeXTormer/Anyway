6e11b7b78317ab59856da6d030773714dfda2fd0
Felix Holz <felix.holz@me.com>
2017-11-08 20:21:11 +0100

    Improved Main Menu
    Former-commit-id: 97dbaaf67ccddff550ab5b5e8ddc92e14a0459a5

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class PlayerController : MonoBehaviour {
 
     public float speed = 2.0f;
@@ -16,6 +17,7 @@ public class PlayerController : MonoBehaviour {
     private float moveInput;
     private float turnInput;
 
+    
 	// Use this for initialization
 	void Awake () {
         body = GetComponent<Rigidbody>();

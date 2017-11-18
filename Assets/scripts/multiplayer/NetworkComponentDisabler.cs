014dec181ac07108ace68eeccbd85e51272c1de2
Felix Holz <felix.holz@me.com>
2017-11-18 21:23:34 +0100

    basic player movement over network
    Former-commit-id: 6ffcf3db1b533ad42eb9bfb8ed6d1e199388cde9

---
+++ Assets/scripts/multiplayer/NetworkComponentDisabler.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+class NetworkComponentDisabler : NetworkBehaviour
+{
+
+    [SerializeField]
+    private Component[] componentsToDisable;
+
+    [SerializeField]
+    private GameObject[] gameObjectsToDisable;
+
+    [SerializeField]
+    private bool localPlayer;
+
+    private void Start()
+    {
+        if(!isLocalPlayer)
+        {
+            localPlayer = false;
+            foreach (Component c in componentsToDisable)
+            {
+                Destroy(c);
+            }
+
+            foreach (GameObject c in gameObjectsToDisable)
+            {
+                Destroy(c);
+            }
+        }
+        else
+        {
+            localPlayer = true;
+        }
+        
+    }
+   
+}

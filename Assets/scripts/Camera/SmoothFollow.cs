eb034fc0a732dfd72ca51e67feeead78d9de789d
Felix Holz <felix.holz@me.com>
2017-11-19 21:38:32 +0100

    smooth camera scrolling
    Former-commit-id: 5f21dd4cc4643284abfc95ac0bf5bb90f26c1835

--- Assets/scripts/Camera/SmoothFollow.cs
+++ Assets/scripts/Camera/SmoothFollow.cs
@@ -17,6 +17,12 @@ class SmoothFollow : MonoBehaviour
     [Tooltip("Vertical distance to the target")]
     public float height = 5;
 
+    [Header("Maximum Values")]
+    public float minDistance = 4;
+    public float maxDistance = 50;
+
+    
+
     public float minHeight = 1;
     public float maxHeight = 50;
 
@@ -34,11 +40,27 @@ class SmoothFollow : MonoBehaviour
     public void FixedUpdate()
     {
         if(caminfo.isActive)
-        { 
+        {
+            float zoom = (-8) * Input.GetAxis("Mouse ScrollWheel");
+            
+            height += zoom;
+            distance += zoom * 2.4f; 
+
+            if (height < minHeight) height = minHeight;
+            if (height > maxHeight) height = maxHeight;
+
+            if (distance < minDistance) distance = minDistance;
+            if (distance > maxDistance) distance = maxDistance;
+            
+            
+
+            this.transform.localPosition = new Vector3(0, height, distance);
+            this.transform.position = new Vector3(this.transform.position.x, height, this.transform.position.z);
+
             Vector3 targetPos = this.transform.position;
             
             caminfo.cameraContainer.transform.position = Vector3.Lerp(caminfo.cameraContainer.transform.position, this.transform.position, Time.deltaTime * damping);
-            caminfo.cameraContainer.transform.position = new Vector3(caminfo.cameraContainer.transform.position.x, player.transform.position.y + height, caminfo.cameraContainer.transform.position.z);
+
 
             caminfo.cameraContainer.transform.LookAt(player.transform);
 

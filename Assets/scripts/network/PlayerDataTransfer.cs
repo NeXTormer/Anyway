8958d258f867fb77d81d2a15919c8e30e96e8e09
Felix Holz <felix.holz@me.com>
2017-12-07 13:55:07 +0100

    removed useless ovr stuff
    Former-commit-id: 998b52be3e22748cdbefe407c025a075cab90bb0

--- Assets/scripts/network/PlayerDataTransfer.cs
+++ Assets/scripts/network/PlayerDataTransfer.cs
@@ -1,14 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Networking;
+﻿using UnityEngine;
 
 public class PlayerDataTransfer : MonoBehaviour {
 
+    public static PlayerDataTransfer instance = null;
 
     public string playerName = "";
     public bool useSteeringWheel = false;
 
+
+    public void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
 
     public void Start()
     {

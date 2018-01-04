5a2f9147cf7147ae10c3a43eb589aee294444d9f
Felix Holz <felix.holz@me.com>
2018-01-04 16:46:09 +0100

    improved raceui
    Former-commit-id: 48e8257604cb707a03dc2ab60ea2098d878e5666

--- Assets/scripts/RaceManager/RaceUI.cs
+++ Assets/scripts/RaceManager/RaceUI.cs
@@ -7,13 +7,18 @@ using System;
 
 [AddComponentMenu("RaceManager/RaceUI")]
 [RequireComponent(typeof(NetworkPlayerData))]
-public class RaceUI : MonoBehaviour {
+public class RaceUI : MonoBehaviour
+{
 
-    public Text racetext;
+
+    public Text usernameText;
+    public Text timeText;
+    public Text lapText;
+    public Text wpText;
+
     public Text titleText;
 
     private GameObject player;
-    private StringBuilder sb;
     private NetworkPlayerData data;
 
     private int m_OldLap = -1;
@@ -21,7 +26,6 @@ public class RaceUI : MonoBehaviour {
 
     void Awake()
     {
-        sb = new StringBuilder(60);
         player = this.transform.root.gameObject;
         data = player.GetComponent<NetworkPlayerData>();
     }
@@ -35,8 +39,6 @@ public class RaceUI : MonoBehaviour {
 
             if (data.currentLap != m_OldLap && data.raceActive)
             {
-                Debug.Log("PETERSCHUB: " + m_OldLap + " | " + data.currentLap);
-
                 m_OldLap = data.currentLap;
 
                 if (m_LapStartTime != 0)
@@ -55,22 +57,10 @@ public class RaceUI : MonoBehaviour {
 
     public void UpdateText()
     {
-        /* TODO: performance? */
-        sb = new StringBuilder(70);
-        sb.AppendLine(PlayerDataTransfer.instance.playerName);
-        sb.Append("Waypoint: ");
-        sb.Append(data.currentWaypoint);
-        sb.Append(" / ");
-        sb.Append(data.numberOfWaypoints);
-        sb.Append("\nLap: ");
-        sb.Append(data.currentLap);
-        sb.Append(" / ");
-        sb.Append(data.numberOfLaps);
-        sb.Append("\n");
-        sb.Append("Time: ");
-        sb.Append(data.raceTime.ToString("0.00"));
-
-        racetext.text = sb.ToString();
+        usernameText.text = PlayerDataTransfer.instance.playerName;
+        timeText.text = data.raceTime.ToString("0.00");
+        lapText.text = data.currentLap + " / " + data.numberOfLaps;
+        wpText.text = data.currentWaypoint + " / " + data.numberOfWaypoints;
 
         titleText.text = data.titleText;
     }

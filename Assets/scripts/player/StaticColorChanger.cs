3594dbbe51a733ba4a9341d057c59cb74dbd5dee
Felix Holz <felix.holz@me.com>
2018-01-16 19:23:01 +0100

    working on main menu
    Former-commit-id: 136f1953e816aac7788feeb0793355624e535da8

---
+++ Assets/scripts/player/StaticColorChanger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class StaticColorChanger : MonoBehaviour
+{
+    public GameObject[] gameobjects;
+
+    public Material material;
+
+    void Start()
+    {
+        foreach (var gameobject in gameobjects)
+        {
+            var rend = gameobject.GetComponent<MeshRenderer>();
+            foreach (var mat in rend.materials)
+            {
+                if (mat.name.Equals("PriCarColor (Instance)"))
+                {
+                    mat.color = material.color;
+                }
+                if (mat.name.Equals("SecCarColor (Instance)"))
+                {
+                    mat.color = material.color;
+                }
+            }
+        }
+    }
+}

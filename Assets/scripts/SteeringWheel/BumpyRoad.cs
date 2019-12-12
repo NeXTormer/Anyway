9d92837d4f38689bbef60be75feb6a8611266d13
Felix Holz <felix.holz@me.com>
2019-12-12 15:28:33 +0100

    working on uneven surface effect
    Former-commit-id: a83349a855b25b067f167353de8569fc61e5ef82

---
+++ Assets/scripts/SteeringWheel/BumpyRoad.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpyRoad : MonoBehaviour {
+
+    public int effectMagnitude = 50;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //if(other.GetComponent<PlayerInputManager>().enabled)
+        {
+            if(LogitechGSDK.LogiIsConnected(0))
+            {
+                float speed = other.gameObject.GetComponentInParent<Rigidbody>().velocity.magnitude * 3.6f;
+                int period = (int) (speed / 80.0f);
+                //Debug.Log("PerioD:" + period);
+                //LogitechGSDK.LogiPlaySurfaceEffect(0, LogitechGSDK.LOGI_PERIODICTYPE_TRIANGLE, effectMagnitude, period);
+                LogitechGSDK.LogiPlayBumpyRoadEffect(0, effectMagnitude);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //if(other.GetComponent<PlayerInputManager>().enabled)
+        {
+            if (LogitechGSDK.LogiIsConnected(0))
+            {
+                //LogitechGSDK.LogiStopSurfaceEffect(0);
+                LogitechGSDK.LogiStopBumpyRoadEffect(0);            
+            }
+        }
+    }
+}
